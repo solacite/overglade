5c909234b0ea0e15a50535d3a64a9b6f612e18c7
solace <143859954+solacite@users.noreply.github.com>
2026-02-02 01:06:21 +0800

    implement screen shake upon using an ability

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -3,12 +3,15 @@ extends Camera2D
 var max_offset = Vector2(20, 20)
 var trauma = 0.4
 
+
 func _ready() -> void:
 	set_process(true)
 
 func _process(delta):
-	shake()
+	pass
 
-func shake():
-	offset.x = randf_range(-max_offset.x, max_offset.x) * trauma
-	offset.y = randf_range(-max_offset.y, max_offset.y) * trauma
+func _shake():
+	for i in range(0, 7):
+		offset.x = randf_range(-max_offset.x, max_offset.x) * trauma
+		offset.y = randf_range(-max_offset.y, max_offset.y) * trauma
+		await get_tree().create_timer(0.1).timeout
