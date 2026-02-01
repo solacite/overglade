d98a9b514fd028e95843a8adc00b4564dcb7bd53
solace <143859954+solacite@users.noreply.github.com>
2026-02-02 04:19:52 +0800

    fix menu screen but the btns showing over credits lwk pmo sl;kfjkdvfjpagewhio;jklrfsior8ri[wjn;sd

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,14 +1,8 @@
 extends Control
-@onready var animPlayer = $"../../AudioStreamPlayer2D"
 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/happy.tscn")
-
-
-func _on_credits_pressed() -> void:
-	animPlayer.play("slide_in")
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
